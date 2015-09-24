e3ec9bb9a3fa53d38cda6ee84a59a3f858292efd
Clark <clarkonicolas@gmail.com>
2015-09-24 15:12:05 -0400

    update #1

--- http_server1.cc
+++ http_server1.cc
@@ -21,6 +21,9 @@ int main(int argc, char * argv[]) {
     int server_port = -1;
     int rc          =  0;
     int sock        = -1;
+    
+    int newsock; // Is this necessary?
+    struct sockaddr_in sa, ca; // Server address, client address?
 
     /* parse command line args */
     if (argc != 3) {
@@ -36,6 +39,7 @@ int main(int argc, char * argv[]) {
     }
 
     /* initialize */
+    /*
     if (toupper(*(argv[1])) == 'K') { 
 	/* UNCOMMENT FOR MINET 
 	 * minet_init(MINET_KERNEL);
@@ -48,15 +52,35 @@ int main(int argc, char * argv[]) {
 	fprintf(stderr, "First argument must be k or u\n");
 	exit(-1);
     }
+    */
 
 
     /* initialize and make socket */
+    char type_ku = argv[1]; // Grabs the 2nd argument, kernel or user
+
+    if(type_ku == 'k') {
+        minet_init(MINET_KERNEL);
+    } else if (type_ku == 'u') {
+        minet_init(MINET_USER);
+    } else {
+        fprintf(stderr, "usage: http_server1 k|u port\n");
+    }
 
     /* set server address*/
+    memset(&sa, 0, sizeof(sa));
+    sa.sin_port = htons(server_port);
+    sa.sin_addr.s_addr = INADDR_ANY;
+    sa.sin_family = AF_INET;
 
     /* bind listening socket */
+    if(minet_bind(sock, &sa) < 0) {
+        fprintf(stderr, "ENTER ERROR MESSAGE HERE\n");
+    }
 
     /* start listening */
+    if(minet_listen(sock, 5) < 0) { // What to put in backlog argument? 5 just placeholder
+        fprintf(stderr, "ENTER ERROR MESSAGE HERE\n");
+    }
 
     /* connection handling loop: wait to accept connection */
 
@@ -78,11 +102,33 @@ int handle_connection(int sock) {
 	"<html><body bgColor=black text=white>\n"		\
 	"<h2>404 FILE NOT FOUND</h2>\n"
 	"</body></html>\n";
+	
+    // Accepting a connection
+        int newsock = minet_accept(sock, 0);
+
     
     /* first read loop -- get request and headers*/
+        char received[BUFSIZE]; // defined as 1024, Takes data in chunks of 1024 bytes?
+        char totalReceived[BUFSIZE * BUFSIZE]; // don't know what size to use, just used large buffer
+        
+        //int minet_read(int fd, char *buf, int len);
+    int bytesRead = minet_read(newsock, bufbuf, BUFSIZE); // Returns number of bytes read
+    int index; // So the same data isn't read over and over
+
+    while(bytesRead > 0 && (index < (BUFSIZE*BUFSIZE))) {
+        //memcpy(destination, source, size)
+        memcpy(totalReceived + index, received, BUFSIZE);
+        index = index + received;
+
+        bytesRead = minet_read(newsock, received, BUFSIZE); // Reads in more bytes
+    }
+
     
     /* parse request to get file name */
     /* Assumption: this is a GET request and filename contains no spaces*/
+    char fileName[FILENAMESIZE]; // defined as 100
+    // how the fuck....
+    // Would involve totalReceived and fileName
 
     /* try opening the file */
 
