6b8dc609a4b58d3a1860086ad2d470502cc20d42
Clark <clarkonicolas@gmail.com>
2015-09-24 20:35:52 -0400

    thursday progression #1

--- http_server1.cc
+++ http_server1.cc
@@ -18,10 +18,11 @@ int handle_connection(int sock);
 int main(int argc, char * argv[]) {
     int server_port = -1;
     int rc          =  0;
-    int sock        = -1;
-    
+    int sock        = -1; // Is this the socket file descriptor?
+
     int newsock; // Is this necessary?
     struct sockaddr_in sa, ca; // Server address, client address?
+
 
     /* parse command line args */
     if (argc != 3) {
@@ -36,24 +37,7 @@ int main(int argc, char * argv[]) {
 	exit(-1);
     }
 
-    /* initialize */
-    /*
-    if (toupper(*(argv[1])) == 'K') { 
-	/* UNCOMMENT FOR MINET 
-	 * minet_init(MINET_KERNEL);
-         */
-    } else if (toupper(*(argv[1])) == 'U') { 
-	/* UNCOMMENT FOR MINET 
-	 * minet_init(MINET_USER);
-	 */
-    } else {
-	fprintf(stderr, "First argument must be k or u\n");
-	exit(-1);
-    }
-    */
-
-
-    /* initialize and make socket */
+    /* initialize and make socket */            
     char type_ku = argv[1]; // Grabs the 2nd argument, kernel or user
 
     if(type_ku == 'k') {
@@ -64,6 +48,10 @@ int main(int argc, char * argv[]) {
         fprintf(stderr, "usage: http_server1 k|u port\n");
     }
 
+    if(sock = minet_socket(SOCK_STREAM) < 0) {
+        fprintf(stderr, "Error making socket\n");
+    }
+    
     /* set server address*/
     memset(&sa, 0, sizeof(sa));
     sa.sin_port = htons(server_port);
@@ -72,15 +60,17 @@ int main(int argc, char * argv[]) {
 
     /* bind listening socket */
     if(minet_bind(sock, &sa) < 0) {
-        fprintf(stderr, "ENTER ERROR MESSAGE HERE\n");
+        fprintf(stderr, "Error binding socket\n");
     }
 
     /* start listening */
-    if(minet_listen(sock, 5) < 0) { // What to put in backlog argument? 5 just placeholder
-        fprintf(stderr, "ENTER ERROR MESSAGE HERE\n");
+    if(minet_listen(sock, 5) < 0) { // What value to put for backlog?
+        fprintf(stderr, "Error listening to socket\n");
     }
 
     /* connection handling loop: wait to accept connection */
+    // add wait to connect
+    // newsock = minet_accept(sockfd, &ca); // Doesn't make sense to accept here? Need to connect then accept?
 
     while (1) {
 	/* handle connections */
@@ -100,46 +90,85 @@ int handle_connection(int sock) {
 	"<html><body bgColor=black text=white>\n"		\
 	"<h2>404 FILE NOT FOUND</h2>\n"
 	"</body></html>\n";
-	
-    // Accepting a connection
-        int newsock = minet_accept(sock, 0);
 
-    
+    // Accepting a connection
+    int newsock = minet_accept(sock, 0);
+
     /* first read loop -- get request and headers*/
-        char received[BUFSIZE]; // defined as 1024, Takes data in chunks of 1024 bytes?
-        char totalReceived[BUFSIZE * BUFSIZE]; // don't know what size to use, just used large buffer
-        
-        //int minet_read(int fd, char *buf, int len);
+    char received[BUFSIZE]; // defined as 1024, Takes data in chunks of 1024 bytes?
+    char totalReceived[BUFSIZE * BUFSIZE]; // don't know what size to use, just used large buffer
+    
+
+    //int minet_read(int fd, char *buf, int len);
     int bytesRead = minet_read(newsock, bufbuf, BUFSIZE); // Returns number of bytes read
-    int index; // So the same data isn't read over and over
+    int index = 0; // So the same data isn't read over and over
 
     while(bytesRead > 0 && (index < (BUFSIZE*BUFSIZE))) {
         //memcpy(destination, source, size)
-        memcpy(totalReceived + index, received, BUFSIZE);
-        index = index + received;
+        memcpy(totalReceived + index, received, bytesRead);
+        index = index + bytesRead;
 
         bytesRead = minet_read(newsock, received, BUFSIZE); // Reads in more bytes
     }
-
     
+    printf("%s\n", totalReceived); // prints the bytes received
+
     /* parse request to get file name */
     /* Assumption: this is a GET request and filename contains no spaces*/
+
     char fileName[FILENAMESIZE]; // defined as 100
-    // how the fuck....
-    // Would involve totalReceived and fileName
+    char * copy = new char[strlen(totalReceived)] // +1? 
+
+    strcpy(copy, totalReceived);
+
+    if(copy[4] == '/') {
+        strcpy(fileName, copy + 5);
+    }
+
+    //////
+    // Add other cases of finding file name
+    //////
 
     /* try opening the file */
+    FILE * fileRequest = 0;
+    int fileNameLength = strlen(fileName);
+
+    fileRequest = fopen(fileName, "rb");
+
+    /////
+    //  Add other cases of fopen
+    /////
+
+    // Reads data of file into buffer
+    char * data = 0;
+
+    // Get length of file to determine size of buffer
+    fseek(fileRequest, 0, SEEK_END);
+    int lengthOfFile = ftell(fileRequest);
+    fseek(fileRequest, 0, SEEK_SET); // Point back to beginning no file
+    
+    data = new char[lengthOfFile];
+    memset(lengthOfFile, 0, lengthOfFile);
+
+    // size_t fread(void * ptr, size_t size, size_t count, FILE * stream)
+    fread(data, 1, lengthOfFile, fileRequest);
+    fclose(fileRequest);
+
 
     /* send response */
     if (ok) {
+
 	/* send headers */
-	
+	//char * toSend = new char[strlen(ok_response_f)];
+    //memset(toSend, 0, strlen(ok_response_f));
+
 	/* send file */
 	
     } else {
 	// send error response
+        minet_write(newsock, notok_response, strlen(notok_response)); // +1 ? 
     }
-    
+
     /* close socket and free space */
   
     if (ok) {
